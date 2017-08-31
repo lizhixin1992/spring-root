3907c8a952db819b95a70725e3f2a917cf735acc
lzx <lzx920911@gmail.com>
2017-08-31 16:20:05 +0800

    demo是简单的发送邮件，demo2是添加图片和附件的发送邮件。现在图片和附件是放到target下，当clear或者install时会清除。

--- spring-web/src/test/com/blue/mail/test/JavaMailDemo2.java
+++ spring-web/src/test/com/blue/mail/test/JavaMailDemo2.java
@@ -1,19 +1,21 @@
 package com.blue.mail.test;
 
+import javax.activation.DataHandler;
+import javax.activation.FileDataSource;
+import javax.mail.Message.RecipientType;
 import javax.mail.Session;
 import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
+import javax.mail.internet.*;
 import java.util.Date;
 import java.util.Properties;
 
 /**
- * @description: 简单的邮件发送
+ * @description: 复杂的邮件
  * @email:
  * @author: lizhixin
- * @createDate: 13:22 2017/8/31
+ * @createDate: 14:27 2017/8/31
  */
-public class JavaMailDemo {
+public class JavaMailDemo2 {
 
     // 发件人的 邮箱 和 密码（替换为自己的邮箱和密码）
     // PS: 某些邮箱服务器为了增加邮箱本身密码的安全性，给 SMTP 客户端设置了独立密码（有的邮箱称为“授权码”）,
@@ -83,8 +85,6 @@ public class JavaMailDemo {
     }
 
     /**
-     * 创建一封只包含文本的简单邮件
-     *
      * @param session 和服务器交互的会话
      * @param sendMail 发件人邮箱
      * @param receiveMail 收件人邮箱
@@ -92,28 +92,66 @@ public class JavaMailDemo {
      * @throws Exception
      */
     public static MimeMessage createMimeMessage(Session session, String sendMail, String receiveMail) throws Exception {
-        // 1. 创建一封邮件
+        // 1. 创建邮件对象
         MimeMessage message = new MimeMessage(session);
 
-        // 2. From: 发件人（昵称有广告嫌疑，避免被邮件服务器误认为是滥发广告以至返回失败，请修改昵称）
-        message.setFrom(new InternetAddress(sendMail, "李志鑫", "UTF-8"));
+        // 2. From: 发件人
+        message.setFrom(new InternetAddress(sendMail, "我的测试邮件_发件人昵称", "UTF-8"));
 
         // 3. To: 收件人（可以增加多个收件人、抄送、密送）
-        message.setRecipient(MimeMessage.RecipientType.TO, new InternetAddress(receiveMail, "lzx", "UTF-8"));
+        message.addRecipient(RecipientType.TO, new InternetAddress(receiveMail, "我的测试邮件_收件人昵称", "UTF-8"));
 
-        // 4. Subject: 邮件主题（标题有广告嫌疑，避免被邮件服务器误认为是滥发广告以至返回失败，请修改标题）
-        message.setSubject("我的测试邮件", "UTF-8");
+        // 4. Subject: 邮件主题
+        message.setSubject("TEST邮件主题（文本+图片+附件）", "UTF-8");
 
-        // 5. Content: 邮件正文（可以使用html标签）（内容有广告嫌疑，避免被邮件服务器误认为是滥发广告以至返回失败，请修改发送内容）
-        message.setContent("这是第一次测试邮件发送。。。。。。。。。。。。。。。。。。。。。。。", "text/html;charset=UTF-8");
+        /*
+         * 下面是邮件内容的创建:
+         */
+        // 5. 创建图片“节点”
+        String classPath = new String(JavaMailDemo2.class.getClass().getResource("/").getPath().getBytes(),"utf-8");
+        MimeBodyPart image = new MimeBodyPart();
+        DataHandler dh = new DataHandler(new FileDataSource(classPath+"57d9f87ae35b91c9306ab4ddd499bc02.jpg")); // 读取本地文件
+        image.setDataHandler(dh);                   // 将图片数据添加到“节点”
+        image.setContentID("image_fairy_tail");     // 为“节点”设置一个唯一编号（在文本“节点”将引用该ID）
 
-        // 6. 设置发件时间
+        // 6. 创建文本“节点”
+        MimeBodyPart text = new MimeBodyPart();
+        //    这里添加图片的方式是将整个图片包含到邮件内容中, 实际上也可以以 http 链接的形式添加网络图片
+        text.setContent("这是一张图片<br/><img src='cid:image_fairy_tail'/>", "text/html;charset=UTF-8");
+
+        // 7. （文本+图片）设置 文本 和 图片 “节点”的关系（将 文本 和 图片 “节点”合成一个混合“节点”）
+        MimeMultipart mm_text_image = new MimeMultipart();
+        mm_text_image.addBodyPart(text);
+        mm_text_image.addBodyPart(image);
+        mm_text_image.setSubType("related");    // 关联关系
+
+        // 8. 将 文本+图片 的混合“节点”封装成一个普通“节点”
+        //    最终添加到邮件的 Content 是由多个 BodyPart 组成的 Multipart, 所以我们需要的是 BodyPart,
+        //    上面的 mm_text_image 并非 BodyPart, 所有要把 mm_text_image 封装成一个 BodyPart
+        MimeBodyPart text_image = new MimeBodyPart();
+        text_image.setContent(mm_text_image);
+
+        // 9. 创建附件“节点”
+        MimeBodyPart attachment = new MimeBodyPart();
+        DataHandler dh2 = new DataHandler(new FileDataSource(classPath+"test.txt"));  // 读取本地文件
+        attachment.setDataHandler(dh2);                                             // 将附件数据添加到“节点”
+        attachment.setFileName(MimeUtility.encodeText(dh2.getName()));              // 设置附件的文件名（需要编码）
+
+        // 10. 设置（文本+图片）和 附件 的关系（合成一个大的混合“节点” / Multipart ）
+        MimeMultipart mm = new MimeMultipart();
+        mm.addBodyPart(text_image);
+        mm.addBodyPart(attachment);     // 如果有多个附件，可以创建多个多次添加
+        mm.setSubType("mixed");         // 混合关系
+
+        // 11. 设置整个邮件的关系（将最终的混合“节点”作为邮件的内容添加到邮件对象）
+        message.setContent(mm);
+
+        // 12. 设置发件时间
         message.setSentDate(new Date());
 
-        // 7. 保存设置
+        // 13. 保存上面的所有设置
         message.saveChanges();
 
         return message;
     }
-
 }
