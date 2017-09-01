8ea8f6e4fd53d045c2ec6074ddc234b8cc208397
lzx <lzx920911@gmail.com>
2017-09-01 17:53:45 +0800

    测试一下ajax请求后台和后台的接收方式

--- spring-web/src/main/java/com/blue/spring/controller/StudentController.java
+++ spring-web/src/main/java/com/blue/spring/controller/StudentController.java
@@ -1,12 +1,15 @@
 package com.blue.spring.controller;
 
+import com.blue.spring.model.ResultVo;
 import com.blue.spring.model.Student;
 import com.blue.spring.service.StudentService;
+import com.sun.net.httpserver.Authenticator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -31,11 +34,19 @@ public class StudentController {
     @RequestMapping("/toStudentView")
 //    @ResponseBody
     public String toStudentView(HttpServletRequest request, Model model){
-        List<Student> studentlist = studentService.findAllStudent(null);
-        logger.info("Student ====" + studentlist.toString());
-        model.addAttribute("studentlist",studentlist);
+//        List<Student> studentlist = studentService.findAllStudent(null);
+//        logger.info("Student ====" + studentlist.toString());
+//        model.addAttribute("studentlist",studentlist);
 //        model.addAttribute("studentlist","aaaaaa");
         return "student/StudentView";
+    }
+
+    @RequestMapping(method = RequestMethod.POST,value = "/create")
+    public String createUser(HttpServletRequest request, String username, String password){
+        String a = request.getParameter("username");
+        System.out.println(username);
+        System.out.println(password);
+        return "index";
     }
 
 }
--- spring-web/src/main/webapp/view/student/StudentView.jsp
+++ spring-web/src/main/webapp/view/student/StudentView.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=utf-8"
-         pageEncoding="utf-8"%>
+         pageEncoding="utf-8" %>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
 <%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>
@@ -11,13 +11,41 @@
 </head>
 <body>
 <h2>这是新的一个页面！</h2>
-${studentlist}
-${studentlist[0].name}
-<c:forEach var="i" items="${studentlist}">
-    1
-</c:forEach>
+<div>
+    ${studentlist}
+    ${studentlist[0].name}
+    <c:forEach var="i" items="${studentlist}">
+        1
+    </c:forEach>
+</div>
+
+<div>
+    <%--<form method="post" action="/student/create.do">--%>
+    <%--用户名：<input id="username" name="username" value="">--%>
+    <%--密码：<input id="password" name="password" value="">--%>
+    <%--<input type="submit" value="提交">--%>
+    <%--</form>--%>
+
+        用户名：<input id="username" name="username" type="text">
+        密码：<input id="password" name="password" type="text">
+        <input type="button" value="提交" onclick="create()">
+</div>
+
 
 </body>
+<script src="<%=request.getContextPath()%>/static/js/jquery/jquery-2.1.0.min.js" type="text/javascript"></script>
 <script>
+    function create() {
+        var username = $("input[name='username']").val();
+        var password = $("#password").val();
+        $.ajax({
+            type: 'POST',
+            url: "../student/create.do",
+            data: {"username": username, "password": password},
+            success: function (data) {
+            },
+        });
+    }
+
 </script>
 </html>
