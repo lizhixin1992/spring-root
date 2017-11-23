86579530dd5f4e46aba3c2fd3b93db386e2b57c9
lzx <lzx920911@gmail.com>
2017-11-23 11:34:16 +0800

    upload

--- spring-model/src/main/java/com/blue/spring/util/ftp/ftpHandler.java
+++ spring-model/src/main/java/com/blue/spring/util/ftp/ftpHandler.java
@@ -1,11 +1,15 @@
 package com.blue.spring.util.ftp;
 
+import org.slf4j.LoggerFactory;
+
 import java.sql.Timestamp;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class ftpHandler implements ftpListener {
+
+    private org.slf4j.Logger logger = LoggerFactory.getLogger(ftpHandler.class);
 
     public ftpHandler() {
 
@@ -16,7 +20,7 @@ public class ftpHandler implements ftpListener {
      * 通知进度事件
      */
     public void processEvent(int process, long fileSize) {
-        System.out.println("进度=========：" + process);
+        logger.info("process:{},fileSize:{}",process,fileSize);
         if (process >= 100) {
             //如果镜像成功了，
             //修改镜像进度
