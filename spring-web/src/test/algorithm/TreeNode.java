4fb079d894f7126140ec32ea0e5654b8ddf6636e
lizhixin <lzx920911@gmail.com>
2018-09-28 09:21:27 +0800

    lintcode66

---
+++ spring-web/src/test/algorithm/TreeNode.java
@@ -0,0 +1,16 @@
+package algorithm;
+
+/**
+ * @ClassName TreeNode
+ * @Description TODO
+ * @Date 2018/9/27 下午5:00
+ **/
+public class TreeNode {
+    public int val;
+    public TreeNode left, right;
+
+    public TreeNode(int val) {
+        this.val = val;
+        this.left = this.right = null;
+    }
+}
--- spring-web/src/test/algorithm/lintCode66.java
+++ spring-web/src/test/algorithm/lintCode66.java
@@ -1,51 +1,70 @@
-//package algorithm;
+package algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * @ClassName lintCode66
+ * @Description TODO
+ * @Date 2018/9/26 下午2:32
+ **/
+
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ * public int val;
+ * public TreeNode left, right;
+ * public TreeNode(int val) {
+ * this.val = val;
+ * this.left = this.right = null;
+ * }
+ * }
+ */
+
+//使用栈实现非递归
+
+public class lintCode66 {
+    /**
+     * @param root: A Tree
+     * @return: Preorder in ArrayList which contains node values.
+     */
+    public List<Integer> preorderTraversal(TreeNode root) {
+        // write your code here
+        //递归版本解法
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        traverse(root, result);
+        return result;
+
+//        //非递归，使用栈解法
+//        List<Integer> res = new ArrayList<>();
+//        Stack<TreeNode> s = new Stack<TreeNode>();
+//        s.add(root);
 //
-//import java.util.ArrayList;
-//import java.util.List;
-//
-///**
-// * @ClassName lintCode66
-// * @Description TODO
-// * @Date 2018/9/26 下午2:32
-// **/
-//
-///**
-// * Definition of TreeNode:
-// * public class TreeNode {
-// *     public int val;
-// *     public TreeNode left, right;
-// *     public TreeNode(int val) {
-// *         this.val = val;
-// *         this.left = this.right = null;
-// *     }
-// * }
-// */
-//
-////使用栈实现非递归
-//
-//public class lintCode66 {
-//    /**
-//     * @param root: A Tree
-//     * @return: Preorder in ArrayList which contains node values.
-//     */
-//    public List<Integer> preorderTraversal(TreeNode root) {
-//        // write your code here
-//        ArrayList<Integer> result = new ArrayList<Integer>();
-//        traverse(root, result);
-//        return result;
-//    }
-//
-//    private void traverse(TreeNode root, ArrayList<Integer> result) {
-//        if (root == null) {
-//            return;
+//        while (!s.isEmpty()) {
+//            TreeNode node = s.pop();
+//            if (node.right != null) {
+//                s.push(node.right);
+//            }
+//            if (node.left != null) {
+//                s.push(node.left);
+//            }
+//            res.add(node.val);
 //        }
-//
-//        result.add(root.val);
-//        traverse(root.left, result);
-//        traverse(root.right, result);
-//    }
-//
-//    public static void main(String[] args) {
-//
-//    }
-//}
+//        return res;
+    }
+
+    private void traverse(TreeNode root, ArrayList<Integer> result) {
+        if (root == null) {
+            return;
+        }
+
+        result.add(root.val);
+        traverse(root.left, result);
+        traverse(root.right, result);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
