d745d963c4d848f0ae5d8541c81ae6a1fda2b2ca
lizhixin <lzx920911@gmail.com>
2018-09-29 12:17:37 +0800

    lintcode69

---
+++ spring-web/src/test/algorithm/lintCode69.java
@@ -0,0 +1,66 @@
+package algorithm;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @ClassName lintCode69
+ * @Description TODO
+ * @Date 2018/9/26 下午2:32
+ **/
+
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ * public int val;
+ * public TreeNode left, right;
+ * public TreeNode(int val) {
+ * this.val = val;
+ * this.left = this.right = null;
+ * }
+ * }
+ */
+
+
+public class lintCode69 {
+    /**
+     * @param root: A Tree
+     * @return: Level order a list of lists of integer
+     */
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        // write your code here
+        List result = new ArrayList();
+
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            ArrayList<Integer> level = new ArrayList<Integer>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode head = queue.poll();
+                level.add(head.val);
+                if (head.left != null) {
+                    queue.offer(head.left);
+                }
+                if (head.right != null) {
+                    queue.offer(head.right);
+                }
+            }
+            result.add(level);
+        }
+
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
