9f29c89738e6f39fcfac959ffdbaa0a8b25ed091
lizhixin <lzx920911@gmail.com>
2018-10-18 09:09:16 +0800

    lintcode93

---
+++ spring-web/src/test/algorithm/lintCode93.java
@@ -0,0 +1,45 @@
+package algorithm;
+
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ * public int val;
+ * public TreeNode left, right;
+ * public TreeNode(int val) {
+ * this.val = val;
+ * this.left = this.right = null;
+ * }
+ * }
+ */
+
+/**
+ * @ClassName lintCode93
+ * @Description TODO
+ * @Date 2018/9/26 下午2:32
+ **/
+public class lintCode93 {
+    /**
+     * @param root: The root of binary tree.
+     * @return: True if this Binary tree is Balanced, or false.
+     */
+    public boolean isBalanced(TreeNode root) {
+        return maxDepth(root) != -1;
+    }
+
+    private int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        if (left == -1 || right == -1 || Math.abs(left-right) > 1) {
+            return -1;
+        }
+        return Math.max(left, right) + 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(0 ^ 7);
+    }
+}
